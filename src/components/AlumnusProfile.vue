2483428e67c9e258918f318380aaec078699235a
MuhammadHafijur <85216180+MuhammadHafijur@users.noreply.github.com>
2024-03-04 13:17:17 +0600

    :fire: setup: Initial setup

---
+++ src/components/AlumnusProfile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    <h4>{{ alumnus.name }}</h4>
+    <p>{{ alumnus.role }}</p>
+    <div class="flex items-center gap-2">
+        <a v-for="socialLink in alumnus.social" :href="socialLink.url" class="text-blue-500 text-base font-semibold">{{ socialLink.name }}</a>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps(['alumnus'])
+
+console.log(props)
+</script>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="font-inter bg-graditent-to-tr">
+    <section class="mx-auto w-full max-w-[1440px] px-4 sm:px-6 md:px-8">
+      <div class="grid grid-cols-4">
+        <AlumnusProfile v-for="alumnus in alumniDB.alumni" :key="alumnus.id" :alumnus="alumnus" />
+      </div>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+import alumniDB from '../db/alumniDB.json'
+
+import AlumnusProfile from '../components/AlumnusProfile.vue'
+</script>
